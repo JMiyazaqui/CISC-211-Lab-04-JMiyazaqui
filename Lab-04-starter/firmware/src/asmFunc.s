cb56a53ea347cd3f9ab9dd25c3babe906298fe6e
JMiyazaqui <miyazaqui.julio@gmail.com>
2025-03-03 14:49:28 -0800

    Update asmFunc.s

--- Lab-04-starter/firmware/src/asmFunc.s
+++ Lab-04-starter/firmware/src/asmFunc.s
@@ -35,7 +35,7 @@ we_have_a_problem: .word     0  /* output value */
 .type nameStr,%gnu_unique_object
     
 /*** STUDENTS: Change the next line to your name!  **/
-nameStr: .asciz "Inigo Montoya"  
+nameStr: .asciz "Julio Miyazaqui"  
 
 .align   /* realign so that next mem allocations are on word boundaries */
  
@@ -75,7 +75,63 @@ asmFunc:
  
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
+    /* Load balance from memory */
+    ldr r1, =balance
+    ldr r2, [r1]
 
+    /* Check if transaction amount in r0 is within range (-1000 to 1000) */
+    mov r3, #1000
+    cmp r0, r3
+    bgt overflow_error
+    rsb r3, r3, #0  /* r3 = -1000 */
+    cmp r0, r3
+    blt overflow_error
+
+    /* Calculate tmpBalance = balance + transaction */
+    adds r4, r2, r0  /* r4 = tmpBalance */
+    bvs overflow_error  /* Branch if overflow occurs */
+
+    /* Store transaction amount */
+    ldr r5, =transaction
+    str r0, [r5]
+
+    /* Store new balance */
+    str r4, [r1]
+
+    /* Set output variables based on new balance */
+    ldr r6, =eat_out
+    ldr r7, =stay_in
+    ldr r8, =eat_ice_cream
+    mov r9, #0
+    str r9, [r6]
+    str r9, [r7]
+    str r9, [r8]
+
+    cmp r4, #0
+    beq set_stay_in
+    bmi set_eat_ice_cream
+    b set_eat_out
+
+set_stay_in:
+    mov r9, #1
+    str r9, [r7]
+    b done
+
+set_eat_ice_cream:
+    mov r9, #1
+    str r9, [r8]
+    b done
+
+set_eat_out:
+    mov r9, #1
+    str r9, [r6]
+    b done
+
+overflow_error:
+    ldr r10, =we_have_a_problem
+    mov r9, #1
+    str r9, [r10]
+    b done
     
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
 
